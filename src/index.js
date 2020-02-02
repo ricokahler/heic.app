df0b1f845292d25ed93e5e11b83565e4ecc0645e
Rico Kahler <ricokahler@me.com>
2020-02-01 23:07:49 -0500

    add sentry

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { useCssReset, ThemeProvider, createTheme } from 'hacker-ui';
+import * as Sentry from '@sentry/browser';
+
 import './index.css';
 import App from './App';
+
+Sentry.init({ dsn: 'https://e0f2505d220c46b29a91ff4b6a2b6bc4@sentry.io/2169844' });
 
 const container = document.createElement('div');
 container.classList.add('container');
