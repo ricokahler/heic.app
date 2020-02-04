5da0f1e44736637370ab9b5324fbee677bd8efe8
Rico Kahler <ricokahler@me.com>
2020-02-04 17:40:38 -0500

    init sentry in worker

--- src/heic.worker.js
+++ src/heic.worker.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-restricted-globals */
+import * as Sentry from '@sentry/browser';
 import heicToJpegDataUrl from './heicToJpegDataUrl';
+
+Sentry.init({
+  dsn: 'https://e0f2505d220c46b29a91ff4b6a2b6bc4@sentry.io/2169844',
+});
 
 self.addEventListener('message', async e => {
   try {
